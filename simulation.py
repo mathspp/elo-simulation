60c7c678bdba3673e86df743951807f99507c0bf
Rodrigo Girão Serrão <rodrigogiraoserrao@gmail.com>
2022-01-25 01:34:04 +0000

    Create players with queues and randomise answering order.

--- simulation.py
+++ simulation.py
@@ -1,6 +1,8 @@
+import random
+
 import streamlit as st
 
-from player import Question, AlwaysRight
+from player import Question, UsuallyRight
 
 
 st.title("ELO-based rating system for quizzes")
@@ -17,12 +19,16 @@ with st.expander("Rating settings:"):
 QUESTIONS = st.slider("Number of questions:", min_value=10, max_value=100, value=20)
 PLAYERS = st.slider("Number of players:", min_value=100, max_value=10_000, value=300)
 
-players = [AlwaysRight(INITIAL_PLAYER_RATING) for _ in range(PLAYERS)]
 questions = [Question(INITIAL_Q_RATING) for _ in range(QUESTIONS)]
+players = []
+for _ in range(PLAYERS):
+    random.shuffle(questions)
+    players.append(UsuallyRight(INITIAL_PLAYER_RATING, questions[::]))
 
-plot = st.line_chart([])
+while players:
+    idx = random.randint(0, len(players) - 1)
+    players[idx].answer_next()
+    if not players[idx].question_queue:
+        players.pop(idx)
 
-for q in questions:
-    for p in players:
-        p.answer_and_update(q)
-    plot.add_rows([q.rating])
+st.line_chart([q.rating for q in questions])
